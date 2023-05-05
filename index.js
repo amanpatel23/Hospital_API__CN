33d2e93dc712846483f223fef0ccbf986bef45b5
amanpatel23 <ap289503@gmail.com>
2023-05-06 02:38:55 +0530

    commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+// defining port
 const port = 8000;
+// getting database connection
 const db = require('./config/mongoose');
 
+// middleware the convert data into json
 app.use(express.json());
+// middleware to decode form data
 app.use(express.urlencoded({
     extended: true
 }));
@@ -11,6 +15,7 @@ app.use(express.urlencoded({
 const myRoute = require('./routes');
 app.use('/', myRoute);
 
+// running express server on the port defined
 app.listen(port, (error) => {
     if (error) console.log(`error in running the server, ${error}`);
     else console.log(`server is running at port: ${port}`);
--- routes/api/v1/doctors.js
+++ routes/api/v1/doctors.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+// getting doctors api from the controllers
 const doctorsApi = require('../../../controllers/api/v1/doctors_api');
 
+// route for handling doctor registration
 router.post('/register', doctorsApi.register);
+// route for handling login of the doctor
 router.post('/login', doctorsApi.login);
 
 module.exports = router;
